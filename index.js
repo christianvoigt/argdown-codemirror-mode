4d714f278509d1f47656858c2e18e469642fd7c6
Christian Voigt <1pxsolidblack@googlemail.com>
2017-11-23 14:06:13 +0100

    added support for tags, fixed inference highlighting

--- index.js
+++ index.js
@@ -1,36 +1,39 @@
 var mode = {
   start: [
-    {regex: /<!--/, token: 'comment', next: 'comment'},
-    {regex: /__.*?__+/, token: 'bold'}, //naive implementation (no ranges within bold range supported)
-    {regex: /\*\*.*?\*\*+/, token: 'bold'}, //naive implementation (no ranges within bold range supported)
-    {regex: /_.*?_+/, token: 'italic'}, //naive implementation (no ranges within italic range supported)
-    {regex: /\*.*?\*+/, token: 'italic'}, //naive implementation (no ranges within italic range supported)
-    {regex: /[ \t]*\(\d+\)/, sol: true, token: 'argument-statement'},
-    {regex: /#+.*/, sol: true, token: 'heading'},
-    {regex: /--/, sol: true, token: 'inference', next: 'inference'},
-    {regex: /\[.+\]\(.+\)/, token: 'link'},
-    {regex: /@\[.+\]/, token: 'statement-mention'},
-    {regex: /\[.+\]:/, token: 'statement-definition'},
-    {regex: /\[.+\]/, token: 'statement-reference'},
-    {regex: /@<.+>/, token: 'argument-mention'},
-    {regex: /<.+>:/, token: 'argument-definition'},
-    {regex: /<.+>/, token: 'argument-reference'},
-    {regex: /[ \t]+\*/, sol: true, token: 'list-item'},
-    {regex: /[ \t]+\d+\./, sol: true, token: 'list-item'},
-    {regex: /[ \t]+></, sol: true, token: 'contradiction'},
-    {regex: /[ \t]+\+>/, sol: true, token: 'incoming-support'},
-    {regex: /[ \t]+->/, sol: true, token: 'incoming-attack'},
-    {regex: /[ \t]+<?\+/, sol: true, token: 'outgoing-support'},
-    {regex: /[ \t]+<?-/, sol: true, token: 'outgoing-attack'}
+    { regex: /<!--/, token: 'comment', next: 'comment' },
+    { regex: /__.*?__+/, token: 'bold' }, //naive implementation (no ranges within bold range supported)
+    { regex: /\*\*.*?\*\*+/, token: 'bold' }, //naive implementation (no ranges within bold range supported)
+    { regex: /_.*?_+/, token: 'italic' }, //naive implementation (no ranges within italic range supported)
+    { regex: /\*.*?\*+/, token: 'italic' }, //naive implementation (no ranges within italic range supported)
+    { regex: /#(?:\([^\)]+\)|[\w-]+)[ \t]?/, token: 'tag' },
+    { regex: /[ \t]*\(\d+\)/, sol: true, token: 'argument-statement' },
+    { regex: /#+.*/, sol: true, token: 'heading' },
+    { regex: /--+/, sol: true, token: 'inference', next: 'inference' },
+    { regex: /\[.+\]\(.+\)/, token: 'link' },
+    { regex: /@\[.+\]/, token: 'statement-mention' },
+    { regex: /\[.+\]:/, token: 'statement-definition' },
+    { regex: /\[.+\]/, token: 'statement-reference' },
+    { regex: /@<.+>/, token: 'argument-mention' },
+    { regex: /<.+>:/, token: 'argument-definition' },
+    { regex: /<.+>/, token: 'argument-reference' },
+    { regex: /[ \t]+\*/, sol: true, token: 'list-item' },
+    { regex: /[ \t]+\d+\./, sol: true, token: 'list-item' },
+    { regex: /[ \t]+></, sol: true, token: 'contradiction' },
+    { regex: /[ \t]+\+>/, sol: true, token: 'incoming-support' },
+    { regex: /[ \t]+->/, sol: true, token: 'incoming-attack' },
+    { regex: /[ \t]+<?\+/, sol: true, token: 'outgoing-support' },
+    { regex: /[ \t]+<?-/, sol: true, token: 'outgoing-attack' },
+    { regex: /[ \t]+<_/, sol: true, token: 'incoming-undercut' },
+    { regex: /[ \t]+_>/, sol: true, token: 'outgoing-undercut' }
   ],
   // The multi-line comment state.
   comment: [
-    {regex: /.*?-->/, token: 'comment', next: 'start'},
-    {regex: /.*/, token: 'comment'}
+    { regex: /.*?-->/, token: 'comment', next: 'start' },
+    { regex: /.*/, token: 'comment' }
   ],
   inference: [
-    {regex: /.*?--+/, token: 'inference', next: 'start'},
-    {regex: /.*/, token: 'inference'}
+    { regex: /.*?--+/, token: 'inference', next: 'start' },
+    { regex: /.*/, token: 'inference' }
   ],
   // The meta property contains global information about the mode. It
   // can contain properties like lineComment, which are supported by
